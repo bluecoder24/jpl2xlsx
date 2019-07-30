22bb103f04f4d3d0ecfc68eca3e2e4ee266474f2
bluecoder24 <jonathan.haist@t-online.de>
2019-07-30 15:14:39 +0200

    Reads jpl files now

--- jpl2xlsx.py
+++ jpl2xlsx.py
@@ -4,64 +4,62 @@
 
 """
 import xlrd
+import os
 exit = 0
 
-def jpl2xlsx(mode=0, source="", target="export.xlsx"):
+def dirsearch(path):
+    """ 
+       Looks for all .jpl files in the path directory
+    """
+    for root, dirs, filenames in os.walk(path):
+        for filename in filenames:
+            if os.path.splitext(filename)[-1] == ".jpl":
+                yield os.path.join(root, filename)
+
+def jpl2xlsx(source=os.getcwd(), target="export.xlsx"):
     """Function which does the magic
         
         Args:
-            mode (int)          How migration is done (0 = all .jpl files in a directory into a new .xlsx, 1 = only single file into new .xlsx)
-            source (String)     Path of the .jpl file(s), can be a file or a directory, default = execution directory
+            source (String)     Directory of the .jpl file(s), default = execution directory
             target (String)     Path & file name of the .xlsx which is created, default = execution directory, file name = export.xlsx
 
         Returns:
             msg (String)        Error or hopefully success message 
     
     """ 
-    msg = "Tragically an error occured! Error Code: 42"
+    files = list(dirsearch(source))  
 
-    return msg
+    print(files)
 
-def cc():
-    """Function to configurate the arguments for the magic function
+    line_counter = 0
 
-    Args:
-        none
-
-    Returns:
-        none
-    
-    """
-def dc():
-   """Function to display the current configuration
-
-    Args:
-        none
-
-    Returns:
-        mode (int)          How migration is done (0 = all .jpl files in a directory into a new .xlsx, 1 = only single file into new .xlsx)
-        source (String)     Path of the .jpl file(s), can be a file or a directory, default = execution directory
-        target (String)     Path & file name of the .xlsx which is created, default = execution directory, file name = export.xlsx
-    
-    """ 
-    return "Error"
+    for file in files:
+        with open(file) as infile:
+            for line in infile:
+                line_counter += 1
+                if line_counter >= 9:
+                    print(line)
+                elif line_counter == 1: 
+                    basename = os.path.basename(file).split(".")
+                    print(basename[0] + "\n")
+                
+            print("----------")
 
 print("--- jpl2xlsx ---\n\nConsole application to migrate .jpl files (d.capture batch) to .xlsx\nVersion: 1.0\nAuthor: Jonathan Haist <jonathan.haist@t-online.de>")
 print("\n- Commands -\n")
-print("go\tRun the migration with the current (or default) configuration")
-print("dc\tDisplay the current configuration")
-print("cc\tChange the configuration\n")
+print("go\tRun the migration in the execution directory")
+print("goc\tRun the migration in a custom directory")
 print("ex\tExit the program\n")
 
 while exit == 0:
     cmd = input("Please enter a command: ").lower()
     if cmd == "go":
-        print(jpl2xlsx(dc()))
-    elif cmd == "dc":
-        dc()
-    elif cmd == "cc":
-        cc()
+        jpl2xlsx()
+    elif cmd == "goc":
+        u_source = input("Enter the source directory: ")
+        u_target = input("Enter the target directory: ")
+        jpl2xlsx(u_source, u_target)
     elif cmd == "ex":
-        raise SystemExit
+        exit = 1
     else:
         print("Please enter a valid command!")
